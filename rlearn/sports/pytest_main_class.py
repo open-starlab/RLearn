1a0a7813edda80299be1e790a11955b9cf89bb90
Kenjiro ide <ide.kenjiro@g.sp.m.is.nagoya-u.ac.jp>
2025-07-04 15:12:16 +0900

    comment out EDMS pytest function

--- rlearn/sports/pytest_main_class.py
+++ rlearn/sports/pytest_main_class.py
@@ -50,41 +50,41 @@ def test_PVS_visualize_data():
     )
 
 
-def test_EDMS_split_mini_data():
-    # test split_data
-    RLearn_Model(
-        state_def="EDMS",
-        input_path=os.getcwd() + "/test/data/dss/preprocess_data/",
-        output_path=os.getcwd() + "/test/data/dss/preprocess_data/split/",
-    ).split_train_test(pytest=True)
+# def test_EDMS_split_mini_data():
+#     # test split_data
+#     RLearn_Model(
+#         state_def="EDMS",
+#         input_path=os.getcwd() + "/test/data/dss/preprocess_data/",
+#         output_path=os.getcwd() + "/test/data/dss/preprocess_data/split/",
+#     ).split_train_test(pytest=True)
 
 
-def test_EDMS_preprocess_data():
-    # test preprocess observation data
-    RLearn_Model(
-        state_def="EDMS",
-        config=os.getcwd() + "/test/config/preprocessing_dssports2020.json",
-        input_path=os.getcwd() + "/test/data/dss/preprocess_data/split/mini",
-        output_path=os.getcwd() + "/test/data/dss_simple_obs_action_seq/split/mini",
-        num_process=5,
-    ).preprocess_observation(batch_size=64)
+# def test_EDMS_preprocess_data():
+#     # test preprocess observation data
+#     RLearn_Model(
+#         state_def="EDMS",
+#         config=os.getcwd() + "/test/config/preprocessing_dssports2020.json",
+#         input_path=os.getcwd() + "/test/data/dss/preprocess_data/split/mini",
+#         output_path=os.getcwd() + "/test/data/dss_simple_obs_action_seq/split/mini",
+#         num_process=5,
+#     ).preprocess_observation(batch_size=64)
 
 
-def test_EDMS_train_data():
-    # test train model
-    RLearn_Model(state_def="EDMS", config=os.getcwd() + "/test/config/exp_config.json").train(
-        exp_name="sarsa_attacker", run_name="test", accelerator="cpu", devices=1, strategy="ddp", mlflow=False
-    )
+# def test_EDMS_train_data():
+#     # test train model
+#     RLearn_Model(state_def="EDMS", config=os.getcwd() + "/test/config/exp_config.json").train(
+#         exp_name="sarsa_attacker", run_name="test", accelerator="cpu", devices=1, strategy="ddp", mlflow=False
+#     )
 
 
-def test_EDMS_visualize_data():
-    # test visualize
-    RLearn_Model(state_def="EDMS").visualize_data(
-        model_name="exp_config",
-        checkpoint_path=os.getcwd() + "/rlearn/sports/output/sarsa_attacker/test/checkpoints/epoch=1-step=2.ckpt",
-        match_id="2022100106",
-        sequence_id=0,
-    )
+# def test_EDMS_visualize_data():
+#     # test visualize
+#     RLearn_Model(state_def="EDMS").visualize_data(
+#         model_name="exp_config",
+#         checkpoint_path=os.getcwd() + "/rlearn/sports/output/sarsa_attacker/test/checkpoints/epoch=1-step=2.ckpt",
+#         match_id="2022100106",
+#         sequence_id=0,
+#     )
 
 
 if __name__ == "__main__":
