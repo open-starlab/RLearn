42c659ab960b82e7dfe69d877d64be6d1308023b
Akshat Garg <garg.akshat@g.sp.m.is.nagoya-u.ac.jp>
2025-07-12 18:12:26 +0900

    Update main_class.py

--- rlearn/sports/main_class.py
+++ rlearn/sports/main_class.py
@@ -1,39 +1,46 @@
 # from /home/c_yeung/workspace6/python/openstarlab/Event/event/sports/soccer/main_class_soccer/main.py
+# from /home/c_yeung/workspace6/python/openstarlab/Event/event/sports/soccer/main_class_soccer/main.py
 from .soccer.main_class_soccer.main import rlearn_model_soccer
 
-
 class RLearn_Model:
-    state_list = ["PVS", "EDMS"]
+    state_list = ["PVS", "EDMS", "PVSS"]
 
     def __new__(cls, state_def, *args, **kwargs):
         if state_def in cls.state_list:
             return rlearn_model_soccer(state_def, *args, **kwargs)
         else:
-            raise ValueError(f"Invalid state_def '{state_def}'. Supported values are: {', '.join(cls.state_list)}")
+            raise ValueError(
+                f"Invalid state_def '{state_def}'. Supported values are: {', '.join(cls.state_list)}"
+            )
 
 
 if __name__ == "__main__":
     import os
 
+    base_path = os.getcwd()
+
     # test split_data
     RLearn_Model(
-        state_def="PVS",
-        input_path=os.getcwd() + "/test/data/datastadium/",
-        output_path=os.getcwd() + "/test/data/datastadium/split/",
+        state_def="PVSS",
+        input_path=os.path.join(base_path, "test/data/datastadium/"),
+        output_path=os.path.join(base_path, "test/data/datastadium/split/"),
     ).split_train_test()
 
     # test preprocess observation data
     RLearn_Model(
-        state_def="PVS",
-        config=os.getcwd() + "/test/config/preprocessing_dssports2020.json",
-        input_path=os.getcwd() + "/test/data/datastadium/split/mini",
-        output_path=os.getcwd() + "/test/data/datastadium_simple_obs_action_seq/split/mini",
+        state_def="PVSS",
+        config=os.path.join(base_path, "test/config/preprocessing_pvss.json"),
+        input_path=os.path.join(base_path, "test/data/datastadium/split/mini"),
+        output_path=os.path.join(base_path, "test/data/datastadium_pvss_obs/split/mini"),
         num_process=5,
     ).preprocess_observation(batch_size=64)
 
     # test train model
-    RLearn_Model(state_def="PVS", config=os.getcwd() + "/test/config/exp_config.json").train(
-        exp_name="sarsa_attacker",
+    RLearn_Model(
+        state_def="PVSS",
+        config=os.path.join(base_path, "test/config/exp_config_qmix.json"),
+    ).train(
+        exp_name="qmix_pvss",
         run_name="test",
         accelerator="gpu",
         devices=1,
@@ -42,10 +49,10 @@ if __name__ == "__main__":
 
     # test visualize
     RLearn_Model(
-        state_def="PVS",
+        state_def="PVSS",
     ).visualize_data(
-        model_name="exp_config",
-        checkpoint_path=os.getcwd() + "/rlearn/sports/output/sarsa_attacker/test/checkpoints/epoch=1-step=2.ckpt",
+        model_name="exp_config_qmix",
+        checkpoint_path=os.path.join(base_path, "rlearn/sports/output/qmix_pvss/test/checkpoints/epoch=1-step=2.ckpt"),
         match_id="2022100106",
         sequence_id=0,
     )
